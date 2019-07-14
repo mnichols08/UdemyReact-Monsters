86f1d56d5bbd251926905813503a39bc1483101c
Mike Nichols <mikenichols410@gmail.com>
2019-07-13 21:20:57 -0400

    Updates

---
+++ precache-manifest.e29a22df92245faa00667334ef908b3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04957484cad854dd684790922c28a20a",
+    "url": "/UdemyReact-Monsters/index.html"
+  },
+  {
+    "revision": "ee4dbaf1b7fd61b650d7",
+    "url": "/UdemyReact-Monsters/static/css/main.b0e0f420.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/UdemyReact-Monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ee4dbaf1b7fd61b650d7",
+    "url": "/UdemyReact-Monsters/static/js/main.14c19753.chunk.js"
+  },
+  {
+    "revision": "7a222e60930ddd718ace",
+    "url": "/UdemyReact-Monsters/static/js/runtime~main.54021a65.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/UdemyReact-Monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
